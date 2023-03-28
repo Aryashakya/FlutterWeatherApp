57a166b4db97a92a5457e43fa25ae64a48562636
Arya Shakya <ryaashakya@gmail.com>
2023-03-28 18:33:43 +0545

    Access weatherapi.com to get weather data

---
+++ lib/api/weather.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+const apiKey = String.fromEnvironment('API_KEY');
+
+Future<Map?> getWeather(String location) async {
+  if (apiKey.isEmpty) {
+    throw AssertionError('API_KEY is not set');
+  }
+  var url = Uri.http(
+      'api.weatherapi.com', '/v1/current.json', {'key': apiKey, 'q': location});
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+    return {
+      'location': jsonResponse['location']['name'],
+      'temp_c': jsonResponse['current']['temp_c'],
+      'temp_text': jsonResponse['current']['condition']['text'],
+      'temp_icon': jsonResponse['current']['condition']['icon']
+    };
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+    return null;
+  }
+}
