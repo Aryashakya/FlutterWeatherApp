5c1f3ae842ba6e89bf54ce8b8bb1369b57ec1607
Arya Shakya <ryaashakya@gmail.com>
2023-03-28 11:29:33 +0545

    Build Help Screen

---
+++ lib/screens/help_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HelpScreen extends StatelessWidget {
+  const HelpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/border.png'), fit: BoxFit.cover),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80.0,
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "We show weather for you",
+                        style: GoogleFonts.lora(
+                          fontSize: 38,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: TextButton(
+                          style: const ButtonStyle(
+                            splashFactory: NoSplash.splashFactory,
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const HomeScreen(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            "Skip > ",
+                            style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.black),
+                          )),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
