3389265b7c11ead9aeec10e2cd3e2937725fb0a4
Arya Shakya <ryaashakya@gmail.com>
2023-03-29 13:40:40 +0545

    Check if device is connected to internet or not

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_weather_app/api/location.dart';
 import 'package:flutter_weather_app/api/weather.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'help_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,28 +17,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
   final TextEditingController _locationController = TextEditingController();
   String buttonText = 'Save';
-  String location = '';
-  String temperatureText = '';
-  String temperatureCelsius = '';
-  String temperatureIconUrl = '';
+  String? location;
+  String? temperatureText;
+  String? temperatureCelsius;
+  String? temperatureIconUrl;
   bool _isLoading = false;
 
   @override
   void dispose() {
     super.dispose();
     _locationController.dispose();
+    _connectivitySubscription.cancel();
   }
 
   @override
   void initState() {
     super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     if (mounted) {
+      print(_connectionStatus.toString());
       setState(() {
         _isLoading = true;
       });
-      if (_locationController.text.isEmpty) {
+      if (_locationController.text.isEmpty && location == null&&
+          _connectionStatus != ConnectivityResult.none) {
         getCurrentData();
       }
     }
@@ -57,6 +70,33 @@ class _HomeScreenState extends State<HomeScreen> {
         temperatureIconUrl = 'http://${data['temp_icon'].substring(2)}';
         _isLoading = false;
       });
+    }
+  }
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+
+    if (_connectionStatus != ConnectivityResult.none &&
+        _locationController.text.isEmpty) {
+      getCurrentData();
     }
   }
 
@@ -82,131 +122,158 @@ class _HomeScreenState extends State<HomeScreen> {
           style: GoogleFonts.lora(fontSize: 32.0),
         ),
       ),
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 30.0),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        if (_locationController.text == location) {
-                          setState(() {
-                            buttonText = 'Update';
-                          });
-                        } else {
-                          setState(() {
-                            buttonText = 'Save';
-                          });
-                        }
-                      },
-                      controller: _locationController,
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
+          child: (_connectionStatus == ConnectivityResult.none)
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.wifi_off_outlined, size: 80),
+                      const SizedBox(
+                        height: 20.0,
                       ),
-                      decoration: InputDecoration(
-                        hintText: "Enter name of location",
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                            width: 0.5,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
+                      Text(
+                        "Please connect to internet",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
                         ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                            width: 0.5,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        filled: true,
-                        contentPadding: const EdgeInsets.all(8.0),
                       ),
-                    ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () async {
-                      if (_locationController.text.isNotEmpty) {
-                        setState(() {
-                          _isLoading = true;
-                        });
-                        Map? data;
-                        data = await getWeather(_locationController.text);
-                        if (data != null) {
-                          print('data: $data');
-                          setState(() {
-                            location = data!['location'];
-                            temperatureText = data['temp_text'];
-                            temperatureCelsius = data['temp_c'].toString();
-                            temperatureIconUrl =
-                                'http://${data['temp_icon'].substring(2)}';
-                            _isLoading = false;
-                          });
-                        }
-                        if (data == null) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content:
-                                      Text('Error: Cannot get temperature.')));
-                          _isLoading = false;
-                        }
-                      }
-                      if (_locationController.text.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content:
-                                    Text('Error: Location Name is empty.')));
-                      }
-                    },
-                    child: Text(buttonText),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 80.0,
-              ),
-              _isLoading
-                  ? const Center(child: CircularProgressIndicator.adaptive())
-                  : Column(
+                )
+              : Column(
+                  children: [
+                    const SizedBox(height: 30.0),
+                    Row(
                       children: [
-                        Text(
-                          location,
-                          style: GoogleFonts.lato(
-                              fontSize: 40, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 40.0,
-                        ),
-                        Text(
-                          '$temperatureCelsius °C',
-                          style: GoogleFonts.lato(fontSize: 24),
-                        ),
-                        const SizedBox(
-                          height: 40.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.network(temperatureIconUrl),
-                            const SizedBox(
-                              width: 20.0,
+                        Expanded(
+                          child: TextField(
+                            onChanged: (value) {
+                              if (_locationController.text == location) {
+                                setState(() {
+                                  buttonText = 'Update';
+                                });
+                              } else {
+                                setState(() {
+                                  buttonText = 'Save';
+                                });
+                              }
+                            },
+                            controller: _locationController,
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
                             ),
-                            Text(
-                              temperatureText,
-                              style: GoogleFonts.lato(fontSize: 20),
+                            decoration: InputDecoration(
+                              hintText: "Enter name of location",
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                  width: 0.5,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                  width: 0.5,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              filled: true,
+                              contentPadding: const EdgeInsets.all(8.0),
                             ),
-                          ],
+                          ),
                         ),
+                        TextButton(
+                          onPressed: () async {
+                            if (_locationController.text.isNotEmpty) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                              Map? data;
+                              data = await getWeather(_locationController.text);
+                              if (data != null) {
+                                print('data: $data');
+                                setState(() {
+                                  location = data!['location'];
+                                  temperatureText = data['temp_text'];
+                                  temperatureCelsius =
+                                      data['temp_c'].toString();
+                                  temperatureIconUrl =
+                                      'http://${data['temp_icon'].substring(2)}';
+                                  _isLoading = false;
+                                });
+                              }
+                              if (data == null) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'Error: Cannot get temperature.')));
+                                setState(() {
+                                  _isLoading = false;
+                                });
+                              }
+                            }
+                            if (_locationController.text.isEmpty) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text(
+                                          'Error: Location Name is empty.')));
+                            }
+                          },
+                          child: Text(buttonText),
+                        )
                       ],
                     ),
-            ],
-          ),
+                    const SizedBox(
+                      height: 80.0,
+                    ),
+                    (_isLoading && location == null)
+                        ? const Center(
+                            child: CircularProgressIndicator.adaptive())
+                        : Column(
+                            children: [
+                              Text(
+                                location!,
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 40.0,
+                              ),
+                              Text(
+                                '$temperatureCelsius °C',
+                                style: GoogleFonts.lato(fontSize: 24),
+                              ),
+                              const SizedBox(
+                                height: 40.0,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.network(temperatureIconUrl!),
+                                  const SizedBox(
+                                    width: 20.0,
+                                  ),
+                                  Text(
+                                    temperatureText!,
+                                    style: GoogleFonts.lato(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                  ],
+                ),
         ),
       ),
     );
