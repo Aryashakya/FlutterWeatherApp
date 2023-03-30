b66101cb3d5d77e1c966b93fbdfaf7ce99cff792
Arya Shakya <ryaashakya@gmail.com>
2023-03-30 17:00:55 +0545

    Minor changes

--- lib/api/location.dart
+++ lib/api/location.dart
@@ -6,20 +6,16 @@ Future<bool> _handleLocationPermission() async {
 
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    print('Location services are disabled. Please enable the services');
     return false;
   }
   permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      print('Location permissions are denied');
       return false;
     }
   }
   if (permission == LocationPermission.deniedForever) {
-    print(
-        'Location permissions are permanently denied, we cannot request permissions.');
     return false;
   }
   return true;
